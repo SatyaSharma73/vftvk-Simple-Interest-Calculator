5b3ef7c5ddbf06d7f8a91b928c35f0e14737d7ab
Satya Sharma <71933842+SatyaSharma73@users.noreply.github.com>
2021-05-04 23:21:42 +0530

    Final commit 2

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+    var principal =  parseInt(document.getElementById("principal").value);
+    if(principal<=0){
+      window.alert("Enter a positive number").location.reload();
+
+    }
+    var rate =  parseInt(document.getElementById("rate").value);
+    var years =  parseInt(document.getElementById("years").value);
+    var interest = (principal * years * rate) / 100;
+
+     var year = new Date().getFullYear()+years;
+
+        document.getElementById("btn").addEventListener("click", function() {
+       document.getElementById("result").innerHTML = "If you deposit "+principal+","+"<br />";
+       document.getElementById("result1").innerHTML = "at an interest rate of "+rate+"%."+"<br />";
+       document.getElementById("result2").innerHTML = "You will receive an amount of "+interest+","+"<br />";
+       document.getElementById("result3").innerHTML = "in the year <mark>"+year+"</mark>";
+
+
+     });
 }
-        
+
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
